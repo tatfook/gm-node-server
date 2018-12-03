f7fd59635794e7b3bd3573707eb8b49e27cbce81
Leopold Chen <placeintime@live.cn>
2018-12-03 17:07:00 +0800

    fix typo

--- config/config.default.js
+++ config/config.default.js
@@ -35,7 +35,7 @@ module.exports = appInfo => {
   // change to your own sequelize configurations
   config.sequelize = {
     dialect: 'mysql',
-    hostname: '127.0.0.1',
+    host: '127.0.0.1',
     port: 3306,
     database: 'gm_dev',
   };
